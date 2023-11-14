4e7094b4f8eb93517ab69555fd6173c08775032f
Kaushik Chakraborty <git@kaushikc.org>
2023-11-14 07:53:44 +0530

    Update default.nix

--- flakes/machines/precision/default.nix
+++ flakes/machines/precision/default.nix
@@ -6,8 +6,20 @@
 
     time.timeZone = "Asia/Kolkata";
 
-    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.defaultLocale = "en_IN";
     i18n.supportedLocales = [ "all" ];
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_IN";
+      LC_IDENTIFICATION = "en_IN";
+      LC_MEASUREMENT = "en_IN";
+      LC_MONETARY = "en_IN";
+      LC_NAME = "en_IN";
+      LC_NUMERIC = "en_IN";
+      LC_PAPER = "en_IN";
+      LC_TELEPHONE = "en_IN";
+      LC_TIME = "en_IN";
+    };
+
 
     nix.extraOptions = ''
       experimental-features = nix-command flakes
