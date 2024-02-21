3e635eeeab78580c5158c21b6be6f9bb38dd92eb
Kaushik Chakraborty <git@kaushikc.org>
2024-02-21 13:11:35 +0530

    mac updates

---
+++ plain-configs/fish/functions/code.fish
@@ -0,0 +1,4 @@
+function code --wraps='/Users/139137/Applications/Visual Studio Code - Insiders.app/Contents/Resources/app/bin/code' --wraps=\~/Applications/Visual\\\ Studio\\\ Code\\\ -\\\ Insiders.app/Contents/Resources/app/bin/code --wraps=\~/Applications/Visual\\\ Studio\\\ Code.app/Contents/Resources/app/bin/code --wraps='/Users/139137/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --description 'alias code /Users/139137/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code'
+  /Users/139137/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code $argv
+        
+end
